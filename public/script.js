596cd9c9ea2d24491c0601559a45ca3553b4cf3a
Bryan Angelo <bryaneangelo@gmail.com>
2025-12-24 18:21:13 +0530

    Add gitignore

---
+++ public/script.js
@@ -0,0 +1,28 @@
+function login() {
+  const role = localStorage.getItem("role");
+
+  fetch("/api/login", {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value,
+      role
+    })
+  })
+  .then(r => r.json())
+  .then(d => {
+    if (!d.success) {
+      error.innerText = "Invalid credentials";
+      return;
+    }
+
+    localStorage.setItem("username", d.username);
+
+    if (d.role === "admin") {
+      location.href = "/admin.html";
+    } else {
+      location.href = "/home.html";
+    }
+  });
+}
